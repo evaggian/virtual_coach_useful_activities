1220eb863d54e707c3da3f866b64cea2dde405f6
Nele Albers <nalbers1@tudelft.nl>
2023-02-09 15:55:00 +0100

    new attempt

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -151,12 +151,12 @@ function send(message) {
 	//var url = "http://rasa:5005";
 	$.ajax({
 
-		//url: url + "/rasa/webhooks/rest/webhook", // tried on server
+		url: url + "/rasa/webhooks/rest/webhook", // tried on server
 		//url: url + "/webhooks/rest/webhook",  // also tried this on server
 		//url: "http://34.159.190.156:5005/webhooks/rest/webhook", // works locally with localhost
 		//url: "http://34.159.190.156/webhooks/rest/webhook", // also does not work on server
 		//url: "http://rasa:5005/webhooks/rest/webhook", // also does not work on server
-		url: "http://rasa_server:5005/webhooks/rest/webhook", // also does not work on server
+		url: "http://rasa_server:5005/webhooks/rest/webhook", // also does not work on server, also does not work on on new server
 		type: "POST",
 		contentType: "application/json",
 		data: JSON.stringify({ message: message, sender: user_id }),
